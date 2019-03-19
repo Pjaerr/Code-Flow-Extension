75a18d8e7432f31c2334c9d2c5511bedc4fbb8de
Pjaerr <washingtonfaulconer@gmail.com>
2019-03-19 20:43:50 +0000

    Tidied up Output.ts

--- src/Output/Output.ts
+++ src/Output/Output.ts
@@ -1,27 +1,20 @@
 import * as vscode from 'vscode';
-import { DataPoint } from '../Data/DataPoint';
+
+import { GetDataPoints } from '../Data/Data';
 import { ConstructWebview } from './ConstructWebview';
-
-let dataPoints: DataPoint[] = [];
-
-//! Temporary test data
-import testData from './testData';
 
 /**
  * Generates a diagram from the DataPoints and their links in the global state.
  */
 export const GenerateDiagram = () => {
-  //   dataPoints = GetDataPoints();
+  const dataPoints = GetDataPoints();
 
-  //   if (dataPoints === undefined || dataPoints.length < 2) {
-  //     return;
-  //   }
-
-  //! Start Test Data
-  dataPoints = testData;
-  //! End Test Data
+  if (dataPoints === undefined || dataPoints.length < 2) {
+    return;
+  }
 
   vscode.window.showInformationMessage('Generating Diagram...');
+
   ConstructWebview('Code Flow Diagram', dataPoints).then(webview => {
     vscode.window.showInformationMessage('Diagram Generated');
   });
