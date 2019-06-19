6f01aaf7dfa37b45da2c57ef649e4fd4b5a9b31f
Pjaerr <joshcharlesjackson@gmail.com>
2019-06-19 11:22:30 +0100

    Wrapper for the vs code input functionality

--- src/Input/AskUserForInput.ts
+++ src/Input/AskUserForInput.ts
@@ -1,5 +1,10 @@
 import * as vscode from 'vscode';
 
+/**
+ * Asks the user for either text or selection input.
+ * @param prompt The message to show the user to prompt them on what to input
+ * @param options An optional list of options, if provided the input will be a vscode quick pick
+ */
 const AskUserForInput = async (prompt: string, options?: string[]) => {
   let response;
 
@@ -7,6 +12,10 @@ const AskUserForInput = async (prompt: string, options?: string[]) => {
     response = await vscode.window.showQuickPick(options, { placeHolder: prompt });
   } else {
     response = await vscode.window.showInputBox({ prompt });
+  }
+
+  if (!response) {
+    throw new Error('You must provide input for this extension to work!');
   }
 
   return response;
