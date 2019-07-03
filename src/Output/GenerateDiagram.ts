ecb095caacfe1ab591ed53bbb587f412f23c789e
Pjaerr <joshcharlesjackson@gmail.com>
2019-07-03 21:19:46 +0100

    Add check to make sure more than 2 data points exist before generating diagram

--- src/Output/GenerateDiagram.ts
+++ src/Output/GenerateDiagram.ts
@@ -8,22 +8,23 @@ import javascript from './WebViewFiles/javascript';
 const GenerateDiagram = () => {
   let dataPoints = GetDataPointsFromDataStorage();
 
-  //Order dataPoints by their orderId
-  dataPoints = dataPoints.sort((pointA, pointB) => pointA.orderId - pointB.orderId);
+  if (dataPoints.length > 1) {
+    //Order dataPoints by their orderId
+    dataPoints = dataPoints.sort((pointA, pointB) => pointA.orderId - pointB.orderId);
 
-  const panel = vscode.window.createWebviewPanel(
-    'Code-Flow-Diagram',
-    'Code Flow - Diagram',
-    vscode.ViewColumn.One,
-    {
-      enableScripts: true
-    }
-  );
+    const panel = vscode.window.createWebviewPanel(
+      'Code-Flow-Diagram',
+      'Code Flow - Diagram',
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true
+      }
+    );
 
-  let dataPointsHTML = '';
+    let dataPointsHTML = '';
 
-  dataPoints.forEach(point => {
-    dataPointsHTML += `
+    dataPoints.forEach(point => {
+      dataPointsHTML += `
         <div class="data-point">
             <span class="data-point-top-section">
                 <h2 class="data-point-name">${point.name}</h2>
@@ -39,17 +40,17 @@ const GenerateDiagram = () => {
         </div>
       `;
 
-    //If this is not the last Data Point, draw an arrow to the next point
-    if (point.orderId < dataPoints.length - 1) {
-      dataPointsHTML += `
+      //If this is not the last Data Point, draw an arrow to the next point
+      if (point.orderId < dataPoints.length - 1) {
+        dataPointsHTML += `
           <svg width="600" height="100">
             <line x1="50%" y1="0" x2="50%" y2="100%" style="stroke:rgb(255,0,0);stroke-width:2" />
           </svg> 
         `;
-    }
-  });
+      }
+    });
 
-  panel.webview.html = `
+    panel.webview.html = `
     <!DOCTYPE html>
       <html>
         <head>
@@ -71,16 +72,21 @@ const GenerateDiagram = () => {
       </html>
   `;
 
-  /*
+    /*
     Listen for messages being posted from our webview. If the message is a string that starts
     with file:// this means it is a local file link that we can open within the editor.
   */
-  panel.webview.onDidReceiveMessage((message: string) => {
-    if (message.startsWith('file://')) {
-      const uri = vscode.Uri.parse(message);
-      vscode.workspace.openTextDocument(uri).then(doc => vscode.window.showTextDocument(doc));
-    }
-  });
+    panel.webview.onDidReceiveMessage((message: string) => {
+      if (message.startsWith('file://')) {
+        const uri = vscode.Uri.parse(message);
+        vscode.workspace.openTextDocument(uri).then(doc => vscode.window.showTextDocument(doc));
+      }
+    });
+  } else {
+    vscode.window.showWarningMessage(
+      'You must have added atleast 2 Data Points to generate a diagram!'
+    );
+  }
 };
 
 export default GenerateDiagram;
