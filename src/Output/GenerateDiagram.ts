f180ea028871a16b8777135742c762b1fd657955
Pjaerr <joshcharlesjackson@gmail.com>
2019-07-03 20:57:23 +0100

    Extract the webview js and css into seperate files

--- src/Output/GenerateDiagram.ts
+++ src/Output/GenerateDiagram.ts
@@ -1,42 +1,14 @@
 import * as vscode from 'vscode';
 import { GetDataPointsFromDataStorage } from '../Data/DataStorage';
-import DataPoint from '../Data/DataPoint';
+
+//Import WebView Files
+import css from './WebViewFiles/css';
+import javascript from './WebViewFiles/javascript';
 
 const GenerateDiagram = () => {
   let dataPoints = GetDataPointsFromDataStorage();
 
-  // TO TEST DIAGRAM
-  //   const dataPoint1 = new DataPoint(
-  //     13,
-  //     'Test DataPoint 1',
-  //     'Some detail about this test data point.',
-  //     'c:/projects/findr/src/frontend/file.js',
-  //     0
-  //   );
-  //   const dataPoint2 = new DataPoint(
-  //     19,
-  //     'Test DataPoint 2',
-  //     'Some detail about this test data point.',
-  //     'c:/projects/findr/src/frontend/file.js',
-  //     1
-  //   );
-  //   const dataPoint3 = new DataPoint(
-  //     168,
-  //     'Test DataPoint 3',
-  //     'Some detail about this test data point.',
-  //     'c:/projects/findr/src/frontend/file.js',
-  //     2
-  //   );
-  //   const dataPoint4 = new DataPoint(
-  //     28,
-  //     'Test DataPoint 4',
-  //     'Some detail about this test data point.',
-  //     'c:/projects/findr/src/frontend/file.js',
-  //     3
-  //   );
-
-  //   let dataPoints = [dataPoint1, dataPoint2, dataPoint3, dataPoint4];
-
+  //Order dataPoints by their orderId
   dataPoints = dataPoints.sort((pointA, pointB) => pointA.orderId - pointB.orderId);
 
   const panel = vscode.window.createWebviewPanel(
@@ -48,18 +20,17 @@ const GenerateDiagram = () => {
     }
   );
 
-  let dataPointsString = '';
+  let dataPointsHTML = '';
 
   dataPoints.forEach(point => {
-    dataPointsString += `
+    dataPointsHTML += `
         <div class="data-point">
             <span class="data-point-top-section">
                 <h2 class="data-point-name">${point.name}</h2>
                 <a class="data-point-file-link" href="${point.file}">
-                    <h4 class="data-point-file">${vscode.workspace.asRelativePath(
-                      point.file,
-                      true
-                    )}:${point.lineNumber}</h4>
+                    <h4 class="data-point-file">
+                    ${vscode.workspace.asRelativePath(point.file, true)}:${point.lineNumber}
+                    </h4>
                 </a>
             </span>
             <span class="data-point-bottom-section">
@@ -78,56 +49,23 @@ const GenerateDiagram = () => {
           <title>CodeFlow Diagram</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <style>
-            body {
-                display: flex;
-                justify-content: center;
-                flex-direction: column;
-                align-items: center;
-            }
-
-            .data-point {
-                width: 600px;
-                height: auto;
-                border: 1px solid white;
-                padding: 10px;
-                margin: 30px;
-                display: flex;
-                flex-direction: column;
-                color: #333;
-                background: #f5f5f5;
-                box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px;
-            }
-
-            .data-point-top-section {
-                width: 100%;
-                padding: initial;
-                border-bottom: 1px solid #333;
-            }
+            ${css}
           </style>
         </head>
         <body>
-            ${dataPointsString}
+            ${dataPointsHTML}
 
             <script>
-            //Grab all links on the page and post them back to be opened in editor when clicked
-            const vscode = acquireVsCodeApi();
-
-            document.addEventListener('click', event => {
-                let node = event && event.target;
-                while (node) {
-                    if (node.tagName && node.tagName === 'A' && node.href) {
-                        vscode.postMessage(node.href);
-                        event.preventDefault();
-                        return;
-                    }
-                    node = node.parentNode;
-                }
-            }, true);
+              ${javascript}
             </script>
         </body>
       </html>
   `;
 
+  /*
+    Listen for messages being posted from our webview. If the message is a string that starts
+    with file:// this means it is a local file link that we can open within the editor.
+  */
   panel.webview.onDidReceiveMessage((message: string) => {
     if (message.startsWith('file://')) {
       const uri = vscode.Uri.parse(message);
---
+++ src/Output/WebViewFiles/javascript.js
@@ -0,0 +1,16 @@
+export default `
+    //Grab all links on the page and post them back to be opened in editor when clicked
+    const vscode = acquireVsCodeApi();
+    
+    document.addEventListener('click', event => {
+        let node = event && event.target;
+        while (node) {
+            if (node.tagName && node.tagName === 'A' && node.href) {
+                vscode.postMessage(node.href);
+                event.preventDefault();
+                return;
+            }
+            node = node.parentNode;
+        }
+    }, true);
+`;
