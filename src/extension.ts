97d37a527e855a79c1fd603c02c5e367ac551f36
Joshua Jackson <joshcharlesjackson@gmail.com>
2020-12-31 20:30:44 +0000

    Setup basic commands
    Create 'add-node' and 'open-diagram' commands and allow them both to be triggered via the editor context menu or the command palette; The 'add-node' option will only be available if there is text selected

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const addNode = vscode.commands.registerCommand('code-flow.add-node', () => {
+    // Triggered via the editor context menu or the command palette when editor has a text selection
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-flow" is now active!');
+    vscode.window.showInformationMessage(
+      "Added node to the diagram, run the 'Open digram' command to view the bigger picture."
+    );
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-flow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  const openDiagram = vscode.commands.registerCommand('code-flow.open-diagram', () => {
+    // Triggered via the editor context menu or the command palette
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-flow!');
-	});
+    vscode.window.showInformationMessage('Opening diagram');
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(addNode);
+  context.subscriptions.push(openDiagram);
 }
 
 // this method is called when your extension is deactivated
